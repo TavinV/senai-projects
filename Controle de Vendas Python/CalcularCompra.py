56465e5d685d1af122a7a7415548e2767b13b2ea
TavinV <otavioviniciusads@gmail.com>
2024-02-19 16:55:03 -0300

    Atualizado funcçoes

--- Controle de Vendas Python/CalcularCompra.py
+++ Controle de Vendas Python/CalcularCompra.py
@@ -1,3 +1,5 @@
+import os
+
 prateleira = {
     "suco de laranja" : 10.50,
     "suco de Uva": 9.99,
@@ -7,16 +9,19 @@ prateleira = {
 carrinho = {}
 
 print("Mercadinho -------------------")
-print("Comprar : Adiciona item ao carrinho \n Retirar : Retira item do carrinho \n Pagar : Imprime a nota fiscal \n Prateleira: Mostra os itens na prateleira e seus preços.")
+print("Comprar : Adiciona item ao carrinho \n Retirar : Retira item do carrinho \n Pagar : Imprime a nota fiscal \n Prateleira: Mostra os itens na prateleira e seus preços. \n Carrinho: mostra o que tem no carrinho.")
 
 while True:
     comando = input("$").lower()
     
-    if comando == "__":
+    if comando == "_k":
+        os.system("cls")
         break
+    
     if comando == "prateleira":
         for Produto, Preco in prateleira.items():
             print(f'{Produto} :  R${Preco} ')
+    
     if comando == "comprar":
         produto = input("Qual o produto? ")
         
@@ -25,9 +30,14 @@ while True:
             
             quantidade = int(input("Quantidade: "))
             
-            carrinho[produto_achado] = quantidade
+            carrinho[produto] = quantidade
             
         except KeyError:
             print("Produto não achado!")
-        print(carrinho)
+    
+    if comando == "carrinho":
+        print("Prod. - Qtde. - Sub Total")
+        for produto, quantidade in carrinho.items():
+            print(carrinho)
+            # print(f'{produto} {quantidade} {(quantidade * prateleira.get(produto))}')
         
