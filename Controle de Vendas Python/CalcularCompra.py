d075d1ccfed31604b3f31bd318d86dcfd436ee84
TavinV <otavioviniciusads@gmail.com>
2024-02-25 19:42:33 -0300

    adicionado funcionalidade dos comandos: retirar, comprar, pagar.

--- Controle de Vendas Python/CalcularCompra.py
+++ Controle de Vendas Python/CalcularCompra.py
@@ -6,10 +6,19 @@ prateleira = {
     "suco de limao" : 8.90
     }
 
-carrinho = {}
 
-print("Mercadinho -------------------")
-print("Comprar : Adiciona item ao carrinho \n Retirar : Retira item do carrinho \n Pagar : Imprime a nota fiscal \n Prateleira: Mostra os itens na prateleira e seus preços. \n Carrinho: mostra o que tem no carrinho.")
+comandos = {
+    "Comprar" : "Adiciona item ao carrinho",
+    "Retirar" : "Retira item do carrinho.",
+    "Pagar" : "Imprime a nota fiscal.",
+    "Prateleira": "Mostra os itens na prateleira e seus preços.",
+    "Carrinho" : "Mostra o que tem no carrinho.",
+}
+
+for comando, descricao in comandos.items():
+    print(f'{comando} : {descricao}\n')
+
+carrinho = {}
 
 while True:
     comando = input("$").lower()
@@ -23,21 +32,57 @@ while True:
             print(f'{Produto} :  R${Preco} ')
     
     if comando == "comprar":
-        produto = input("Qual o produto? ")
+        produto = input("Produto: ")
         
         try: 
-            produto_achado = prateleira[produto]
-            
+            preco_produto = prateleira[produto]
+        
             quantidade = int(input("Quantidade: "))
-            
-            carrinho[produto] = quantidade
+        
+            if quantidade >= 1: # Impede o uso de um valor negativo 
+                if produto in carrinho.keys(): # Caso o produto seja encontrado no carrinho
+                    carrinho[produto] += quantidade
+                else:
+                    carrinho[produto] = quantidade
             
         except KeyError:
             print("Produto não achado!")
     
+    if comando == "retirar":
+        produto_a_remover = input("Produto a remover: ").lower()
+        quantidade = input("Unidades a remover (deixar em 0 cancela, a letra T retira todas.) ").lower()
+        
+        print(produto_a_remover)
+        try:
+            if quantidade == "t":
+                carrinho.pop(produto_a_remover)
+            elif int(quantidade) and int(quantidade) >= 1:
+                carrinho[produto_a_remover] -= int(quantidade)
+
+        except KeyError:
+            print(f'O produto {produto_a_remover} não foi achado.')
+
+        except ArithmeticError:
+            print(f'O numero "{quantidade}" é invalido.')
+
     if comando == "carrinho":
         print("Prod. - Qtde. - Sub Total")
         for produto, quantidade in carrinho.items():
-            print(carrinho)
-            # print(f'{produto} {quantidade} {(quantidade * prateleira.get(produto))}')
+            print(f'{produto} - {quantidade} - {(quantidade * prateleira.get(produto))}')
+
+    if comando == "pagar":
+        cabeçalho = "NOTA FISCAL_________________________"
         
+        print(cabeçalho)
+        
+        total = 0
+        for produto, quantidade in carrinho.items():
+            preco_do_item = quantidade * prateleira.get(produto)
+
+            print(f'{produto} - {quantidade} * {quantidade} R$({preco_do_item})')
+            
+            total += preco_do_item
+        
+        print(f'SEU TOTAL FOI: R${total}')
+
+    print("\n")
