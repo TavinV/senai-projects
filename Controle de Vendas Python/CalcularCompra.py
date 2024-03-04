790034664c37b49db5c8dcb29923d7861abf2fe2
TavinV <otavioviniciusflauzino@gmail.com>
2024-03-04 16:59:20 -0300

    mudado frete

--- Controle de Vendas Python/CalcularCompra.py
+++ Controle de Vendas Python/CalcularCompra.py
@@ -1,4 +1,5 @@
 import os
+from CalcularFrete import calcular_frete
 
 prateleira = {
     "suco de laranja" : 10.50,
@@ -21,68 +22,80 @@ for comando, descricao in comandos.items():
 
 carrinho = {}
 
-while True:
-    comando = input("$").lower()
-    
-    if comando == "_k":
-        os.system("cls")
-        break
-    
-    if comando == "prateleira":
-        for Produto, Preco in prateleira.items():
-            print(f'{Produto} :  R${Preco} ')
-    
-    if comando == "comprar":
-        produto = input("Produto: ")
+def main():
+    while True:
+        comando = input("$").lower()
         
-        try: 
-            preco_produto = prateleira[produto]
+        if comando == "_k":
+            os.system("cls")
+            break
         
-            quantidade = int(input("Quantidade: "))
+        if comando == "prateleira":
+            for Produto, Preco in prateleira.items():
+                print(f'{Produto} :  R${Preco} ')
         
-            if quantidade >= 1: # Impede o uso de um valor negativo 
-                if produto in carrinho.keys(): # Caso o produto seja encontrado no carrinho
-                    carrinho[produto] += quantidade
-                else:
-                    carrinho[produto] = quantidade
+        if comando == "comprar":
+            produto = input("Produto: ")
             
-        except KeyError:
-            print("Produto não achado!")
-    
-    if comando == "retirar":
-        produto_a_remover = input("Produto a remover: ").lower()
-        quantidade = input("Unidades a remover (deixar em 0 cancela, a letra T retira todas.) ").lower()
-        
-        try:
-            if quantidade == "t":
-                carrinho.pop(produto_a_remover)
-            elif int(quantidade) and int(quantidade) >= 1:
-                carrinho[produto_a_remover] -= int(quantidade)
-
-        except KeyError:
-            print(f'O produto {produto_a_remover} não foi achado.')
-
-        except ArithmeticError:
-            print(f'O numero "{quantidade}" é invalido.')
-
-    if comando == "carrinho":
-        print("Prod. - Qtde. - Sub Total")
-        for produto, quantidade in carrinho.items():
-            print(f'{produto} - {quantidade} - {(quantidade * prateleira.get(produto))}')
-
-    if comando == "pagar":
-        cabeçalho = "NOTA FISCAL_________________________"
-        
-        print(cabeçalho)
-        
-        total = 0
-        for produto, quantidade in carrinho.items():
-            preco_do_item = quantidade * prateleira.get(produto)
-
-            print(f'{produto} - {quantidade} * {quantidade} R$({preco_do_item})')
+            try: 
+                preco_produto = prateleira[produto]
             
-            total += preco_do_item
+                quantidade = int(input("Quantidade: "))
+            
+                if quantidade >= 1: # Impede o uso de um valor negativo 
+                    if produto in carrinho.keys(): # Caso o produto seja encontrado no carrinho
+                        carrinho[produto] += quantidade
+                    else:
+                        carrinho[produto] = quantidade
+                
+            except KeyError:
+                print("Produto não achado!")
         
-        print(f'SEU TOTAL FOI: R${total}')
+        if comando == "retirar":
+            produto_a_remover = input("Produto a remover: ").lower()
+            quantidade = input("Unidades a remover (deixar em 0 cancela, a letra T retira todas.) ").lower()
+            
+            try:
+                if quantidade == "t":
+                    carrinho.pop(produto_a_remover)
+                elif int(quantidade) and int(quantidade) >= 1:
+                    carrinho[produto_a_remover] -= int(quantidade)
 
-    print("\n")
+            except KeyError:
+                print(f'O produto {produto_a_remover} não foi achado.')
+
+            except ArithmeticError:
+                print(f'O numero "{quantidade}" é invalido.')
+
+        if comando == "carrinho":
+            print("Prod. - Qtde. - Sub Total")
+            for produto, quantidade in carrinho.items():
+                print(f'{produto} - {quantidade} - {(quantidade * prateleira.get(produto))}')
+
+        if comando == "pagar":
+            cabeçalho = "NOTA FISCAL_________________________"
+            
+            print(cabeçalho)
+            
+            total = 0
+            for produto, quantidade in carrinho.items():
+                preco_do_item = quantidade * prateleira.get(produto)
+
+                print(f'{produto} - {quantidade} * {quantidade} R$({preco_do_item})')
+                
+                total += preco_do_item
+            
+            print(f'SEU TOTAL FOI: R${total}')
+            print("\n")
+            
+            endereco = input("Qual seu endereço, para o calculo do frete? :")
+            distancia = input("Qual a distancia? :")
+            
+            valor_do_frete, taxa_do_frete = calcular_frete(int(distancia), total)
+            
+            print("FRETE______________________________________")
+
+        print("\n")
+
+if __name__ == "__main__":
+    main()
--- Controle de Vendas Python/CalcularFrete.py
+++ Controle de Vendas Python/CalcularFrete.py
@@ -1,1 +1,46 @@
-print("frete")
+# valor_do_frete 
+# distancia em KM em relação ao valor em R$
+ 
+tabela_de_valores_do_frete = {
+    10 : 20,
+    20 : 30,
+    30 : 40,
+    40 : 50,
+}
+
+def calcular_taxa_do_frete(distancia: int) ->  int:
+    """
+    Calcula a taxa do frete baseado na ditancia
+    
+        Inputs:
+            int distancia : A distancia em KM do cliente, caso seja menor que 0 ou superior a 40, o frete sera cancelado.
+        Outputs:
+            int valor_a_pagar_frete : O valor em R$ a pagar pelo frete.
+    """
+     
+    valor_a_pagar_frete = 0
+    
+    distancia_anterior = 10;
+    
+    for distancia_maxima, valor_em_reais in tabela_de_valores_do_frete.items():
+        if distancia <= distancia_maxima and distancia >= distancia_anterior:
+            valor_a_pagar_frete = valor_em_reais
+        elif distancia < 10:
+            valor_a_pagar_frete = tabela_de_valores_do_frete[10]
+        distancia_anterior = distancia_maxima
+        
+    
+    return valor_a_pagar_frete
+            
+
+def calcular_frete(distancia : int, valor_compra: int):
+    porcentagem = 5 # taxa em porcentagem a ser adicionada ao valor do frete
+    valor_da_taxa = (porcentagem / 100) * valor_compra
+    
+    valor_total = valor_da_taxa + calcular_taxa_do_frete(distancia)
+    
+    return (valor_total, valor_da_taxa)
+
+if __name__ == "__main__":
+    resultado = calcular_taxa_do_frete(5)
+    print(resultado)
